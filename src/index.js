741a83f471150956a6ada1216c1f4e91dc99b395
Karan Kumar Gautam <karankumargautam.dev@gmail.com>
2024-07-21 10:57:32 +0530

    bug-fixed: app import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config();
-const app = express();
 const PORT = process.env.PORT || 8080;
 
 connectDB()
