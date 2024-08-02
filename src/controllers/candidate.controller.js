ad27aebbbfef669ae8a59d5c46273f62b8a094b0
Karan Kumar Gautam <karankumargautam.dev@gmail.com>
2024-08-02 12:18:43 +0530

    feat: update candidate details.
    - only admin updateCandidate details.
    - check user is admin
    - get candidateID from req.params
    - check candidateID is valid.
    - get `name, party and age` form frontend.
    - check is not empty.
    - set update candidate detail in database.
    - send response

--- src/controllers/candidate.controller.js
+++ src/controllers/candidate.controller.js
@@ -1,5 +1,5 @@
 import Candidate from "../models/candidate.model.js";
-
+import mongoose from "mongoose";
 const checkAdminRole = (user) => user.role === "admin";
 
 const createNewCandidate = async (req, res) => {
@@ -31,4 +31,56 @@ const createNewCandidate = async (req, res) => {
   }
 };
 
-export { createNewCandidate };
+const updatedCandidate = async (req, res) => {
+  try {
+    if (!checkAdminRole(req.user)) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    const candidateID = req.params.candidateID;
+
+    // check id empty candidateID
+    if (!candidateID) {
+      return res.status(400).json({ message: "Candidate ID is required" });
+    }
+
+    // check mongoose object id is valid
+    if (!mongoose.Types.ObjectId.isValid(candidateID)) {
+      return res.status(400).json({ message: "Invalid Candidate ID" });
+    }
+
+    const { name, party, age } = req.body;
+    if ([name, party, age].some((field) => !field)) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+
+    const response = await Candidate.findByIdAndUpdate(
+      candidateID,
+      {
+        name,
+        party,
+        age,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    // check if not response
+    if (!response) {
+      return res.status(404).json({ error: "Candidate not found" });
+    }
+
+    // send proper res.
+    res.status(200).json({
+      message: "condidate data updated",
+      response: response,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export { createNewCandidate, updatedCandidate };
--- src/routes/candidate.routes.js
+++ src/routes/candidate.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { createNewCandidate } from "../controllers/candidate.controller.js";
+import {
+  createNewCandidate,
+  updatedCandidate,
+} from "../controllers/candidate.controller.js";
 
 const router = Router();
 
 router.route("/").post(verifyJWT, createNewCandidate);
+router.route("/:candidateID").put(verifyJWT, updatedCandidate);
 
 export default router;
