03f1a060e53586271d94fc2fc1db2f34bbdb6aee
gautamkaran <karankumargautam.dev@gmail.com>
2024-09-29 13:52:42 +0530

    Add Signin, Login, Logout Docs

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import swaggerUi from "swagger-ui-express";
+import path from "path";
+import YAML from "yaml";
+import { fileURLToPath } from "url";
+import fs from "fs";
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const file = fs.readFileSync(path.resolve(__dirname, "./swagger.yaml"), "utf8");
+const swaggerDocument = YAML.parse(
+  file?.replace(
+    "- url: ${{server}}",
+    `- url: ${process.env.VOTING_API_HOST_URL || "http://localhost:8080"}/api/v1`
+  )
+);
 
 // cors setup here
 app.use(
@@ -25,5 +39,17 @@ import condidateRouter from "./routes/candidate.routes.js";
 // routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/candidate", condidateRouter);
+
+// for swagger docs
+app.use(
+  "/",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: {
+      docExpansion: "none", // keep all the sections collapsed by default
+    },
+    customSiteTitle: "Voting-APIs docs",
+  })
+);
 
 export { app };
